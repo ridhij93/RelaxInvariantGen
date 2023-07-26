aa4d7d0be7af6030b8c0a03dbc9b4a8501be4dae
ridhijain <ridhij@iiitd.ac.in>
2023-07-26 15:30:19 +0400

    fixes with wrw+2w

--- MainAnalysis.cpp
+++ MainAnalysis.cpp
@@ -2134,7 +2134,7 @@ bool instHasCommonWrite(Instruction * i1, Instruction * i2)
 }
 
 bool isAddressinvalid(void* ptr) {
-    return (reinterpret_cast<uintptr_t>(ptr) < 0xaaaa);
+    return ((reinterpret_cast<uintptr_t>(ptr) < (reinterpret_cast<uintptr_t>(&main_M)) )|| (reinterpret_cast<uintptr_t>(ptr) > 0xfffffffffffa));
 }
 
 bool redundantTrace(vector<vector<Inst>> vec, vector<Inst> target) {
@@ -2202,7 +2202,7 @@ bool redundantTrace(vector<vector<Inst>> vec, vector<Inst> target) {
       }
       // errs() <<"ENTER 0 "<< inst1.bbl_bfs_index<< " "<<inst1.index <<" "<<  isAddressinvalid(inst1.func)<< " "<< inst1.func->getName() << "\n";
 
-      // errs() <<"ENTER 1 "<<inst1.func<<"\n";
+      errs() <<"ENTER 1 "<<inst1.func<<"\n";
 
       if ((main_M->getFunction(inst1.func->getName()) == nullptr))
       {
@@ -2217,7 +2217,7 @@ bool redundantTrace(vector<vector<Inst>> vec, vector<Inst> target) {
       }
       
       
-      // errs() <<"ENTER 2 \n"; 
+      errs() <<"ENTER 2 \n"; 
       if (inst1.index == inst2.index && inst1.bbl_bfs_index == inst2.bbl_bfs_index && inst1.func->getName() == inst2.func->getName())
       {
         i1++;
@@ -2237,10 +2237,10 @@ bool redundantTrace(vector<vector<Inst>> vec, vector<Inst> target) {
         ucount++;
         break;
       } 
-      // errs() <<"ENTER 1.0 \n";
+      errs() <<"ENTER 1.0 \n";
       if ((main_M->getFunction(inst1.func->getName()) == nullptr))
       {
-        // errs() <<"ENTER 1.3 \n";
+        errs() <<"ENTER 1.3 \n";
         if ((main_M->getFunction(inst2.func->getName()) != nullptr))
         {
           ucount++;
@@ -2250,7 +2250,7 @@ bool redundantTrace(vector<vector<Inst>> vec, vector<Inst> target) {
         else 
           return true;
       }
-      // errs() <<"ENTER 1.5 \n";
+      errs() <<"ENTER 1.5 \n";
       if ((main_M->getFunction(inst2.func->getName()) == nullptr))
       {
         return true;
@@ -2300,7 +2300,7 @@ bool redundantTrace(vector<vector<Inst>> vec, vector<Inst> target) {
             ucount++;
             break;
           }
-          // errs() <<"ENTER 1.6 "<< ins_temp.func<<"\n";
+          errs() <<"ENTER 1.6 "<< ins_temp.func <<"\n";
           if (ins_temp.func == nullptr || isAddressinvalid(ins_temp.func))
           {
             ucount++;
@@ -2312,7 +2312,7 @@ bool redundantTrace(vector<vector<Inst>> vec, vector<Inst> target) {
             break;
           }
           Instruction * instruction_temp = getInstruction(ins_temp.index, ins_temp.bbl_bfs_index, ins_temp.func);
-          // errs() <<"ENTER 14\n";
+          errs() <<"ENTER 14\n";
           if (instHasCommonWrite(instruction1, instruction_temp))
           {
             ucount++;
@@ -4576,10 +4576,12 @@ void initGlobalClock()
 
 
 
-
+// 0x55a5b1989750
 void visitor(Module &M) {
+  
   main_M = &M;
   initGlobalClock();
+  // errs() << "BASE address " <<&M << "--" << M.size()<< "\n";
   auto itr = M.functions().begin();
   int func_inst;
   auto itr_func = M.functions().begin();
